112ca1583f1e83de7830850e52d1282a1284c88a
Purna Gurung <purnagurung903@gmail.com>
2021-02-20 11:19:33 -0500

    Added a folder

---
+++ server.js
@@ -0,0 +1,27 @@
+// tracking the cheap flight
+
+
+
+
+// round trip
+
+
+
+
+
+
+// near by airport
+
+
+// Track a particular airplane using flight number
+// Installation 
+// Database - schema
+// Connection - server
+// Connection - database
+// Flight API - comparing round trip prices
+// Flight API - finding nearby API
+// Flight API - Track a particular airplane using flight number API
+
+
+
+
